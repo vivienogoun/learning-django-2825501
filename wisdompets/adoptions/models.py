164c5341d336c6bcfdad0d13f567608ed54a0eec
Vivien Ogoun <vivien.ogoun@epitech.eu>
2025-07-31 16:15:14 +0100

    Implement pet adoption admin interface and data loading command

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+class Pet(models.Model):
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')] # first element is value stored in database, second is display name
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True) # used null=True to allow for unknown ages. blank=True would have stored 0, which could be misleading
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
